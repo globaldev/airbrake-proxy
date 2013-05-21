b4dd1e16b07c5197cfa1478613e769864a2c57a4
Matthew Ward <mattward41@gmail.com>
2013-05-21 20:33:27 +0100

    Improve syntax based on JSHint recommendations

--- server.js
+++ server.js
@@ -37,27 +37,27 @@ try {
 }
 
 // Listen process host and port settings and validation
-config.listen = (config.listen == null) ? {'host': null, 'port': null} : config.listen;
-config.listen.host = (config.listen.host == null) ? "0.0.0.0" : config.listen.host;
-config.listen.port = (config.listen.port == null) ? 80 : parseInt(config.listen.port);
-config.listen.workers = ((config.listen.workers == null) || (config.listen.workers == "cpu")) ? workers : parseInt(config.listen.workers);
-config.listen.hostname = (config.listen.hostname == null) ? "localhost" : config.listen.hostname;
+config.listen = (config.listen === null) ? {'host': null, 'port': null, 'workers': null, 'hostname': null} : config.listen;
+config.listen.host = (config.listen.host === null) ? "0.0.0.0" : config.listen.host;
+config.listen.port = (config.listen.port === null) ? 80 : parseInt(config.listen.port, 10);
+config.listen.workers = ((config.listen.workers === null) || (config.listen.workers === "cpu")) ? workers : parseInt(config.listen.workers, 10);
+config.listen.hostname = (config.listen.hostname === null) ? "localhost" : config.listen.hostname;
 
 if ((config.listen.port < 1) || (config.listen.port > 65535) || isNaN(config.listen.port)) {
 	util.log("Listen port number seems invalid, check configuration");
 	process.exit(2);
 }
 
-if ((config.listen.workers == 0) || isNaN(config.listen.workers)) {
+if ((config.listen.workers === 0) || isNaN(config.listen.workers)) {
 	util.log("Cluster worker count is 0, so no workers would be created");
-	process.exit(3)
+	process.exit(3);
 }
 
 // Configure the airbrake service hostname
-config.airbrake = (config.airbrake == null) ? {'host': null, 'port': null} : config.airbrake;
-config.airbrake.host = (config.airbrake.host == null) ? "api.airbrake.io" : config.airbrake.host.replace(/https?:\/\//, '');
-config.airbrake.port = (config.airbrake.port == null) ? 443 : parseInt(config.airbrake.port);
-config.airbrake.timeout = ((config.airbrake.timeout == null) || (parseInt(config.airbrake.timeout) == 0)) ? 10000 : parseInt(config.airbrake.timeout);
+config.airbrake = (config.airbrake === null) ? {'host': null, 'port': null, 'timeout': null} : config.airbrake;
+config.airbrake.host = (config.airbrake.host === null) ? "api.airbrake.io" : config.airbrake.host.replace(/https?:\/\//, '');
+config.airbrake.port = (config.airbrake.port === null) ? 443 : parseInt(config.airbrake.port, 10);
+config.airbrake.timeout = ((config.airbrake.timeout === null) || (parseInt(config.airbrake.timeout, 10) === 0)) ? 10000 : parseInt(config.airbrake.timeout, 10);
 
 if ((config.airbrake.port < 1) || (config.airbrake.port > 65535) || isNaN(config.airbrake.port)) {
 	util.log("Airbrake port number seems invalid, check configuration");
@@ -65,17 +65,17 @@ if ((config.airbrake.port < 1) || (config.airbrake.port > 65535) || isNaN(config
 }
 
 // Choose the protocol to connect to Airbrake to based on the port
-if (config.airbrake.port == 443) {
+if (config.airbrake.port === 443) {
 	config.airbrake.protocol = require('https');
 } else {
 	config.airbrake.protocol = require('http');
 }
 
 // Redis host and port settings and validation
-config.redis = (config.redis == null) ? {'host': null, 'port': null, 'prefix': null} : config.redis;
-config.redis.host = (config.redis.host == null) ? "127.0.0.1" : config.redis.host;
-config.redis.port = (config.redis.port == null) ? 6379 : parseInt(config.redis.port);
-config.redis.prefix = (config.redis.prefix == null) ? "airbrakeproxy" : config.redis.prefix;
+config.redis = (config.redis === null) ? {'host': null, 'port': null, 'prefix': null} : config.redis;
+config.redis.host = (config.redis.host === null) ? "127.0.0.1" : config.redis.host;
+config.redis.port = (config.redis.port === null) ? 6379 : parseInt(config.redis.port, 10);
+config.redis.prefix = (config.redis.prefix === null) ? "airbrakeproxy" : config.redis.prefix;
 config.redis.key = config.redis.prefix + ":uuid";
 
 if ((config.redis.port < 1) || (config.redis.port > 65535) || isNaN(config.redis.port)) {
@@ -91,10 +91,10 @@ redis.on('error', function (error) {
 });
 
 // StatsD configuration and validation
-config.statsd = (config.statsd == null) ? {'host': null, 'port': null, 'prefix': null} : config.statsd;
-config.statsd.host = (config.statsd.host == null) ? "127.0.0.1" : config.statsd.host;
-config.statsd.port = (config.statsd.port == null) ? 8125 : parseInt(config.statsd.port);
-config.statsd.prefix = (config.statsd.prefix == null) ? "airbrakeproxy" : config.statsd.prefix;
+config.statsd = (config.statsd === null) ? {'host': null, 'port': null, 'prefix': null} : config.statsd;
+config.statsd.host = (config.statsd.host === null) ? "127.0.0.1" : config.statsd.host;
+config.statsd.port = (config.statsd.port === null) ? 8125 : parseInt(config.statsd.port, 10);
+config.statsd.prefix = (config.statsd.prefix === null) ? "airbrakeproxy" : config.statsd.prefix;
 
 if ((config.statsd.port < 1) || (config.statsd.port > 65535) || isNaN(config.statsd.port)) {
 	util.log("StatsD port number seems invalid, check configuration");
@@ -102,7 +102,7 @@ if ((config.statsd.port < 1) || (config.statsd.port > 65535) || isNaN(config.sta
 }
 
 // Create StatsD connection
-statsd = new nodestatsd(config.statsd.host, config.statsd.port);
+var statsd = new nodestatsd(config.statsd.host, config.statsd.port);
 
 // Airbrake style XML response to send to client
 var xmlresponse = '<?xml version="1.0"?><notice><id>{UUID}</id><url>http://' + config.listen.hostname + ':' + config.listen.port + '/locate/{UUID}</url></notice>';
@@ -114,7 +114,7 @@ var xmlresponse = '<?xml version="1.0"?><notice><id>{UUID}</id><url>http://' + c
 if (cluster.isMaster) {
 	var fork = function () {
 		cluster.fork();
-	}
+	};
 
 	// Create enough workers to satisfy passed worker count
 	for (var child = 0; child < config.listen.workers; child++) {
@@ -185,14 +185,14 @@ if (cluster.isMaster) {
 		// Send the Airbrake XML data and end the connection
 		airbrakeRequest.write(data);
 		airbrakeRequest.end();
-	}
+	};
 
 	// Create an HTTP server to listen to lookup GET requests and Airbrake client POST requests
 	http.createServer(function (request, response) {
 		var startHTTP = microtime.now();
 
-		if (request.method == "GET") {
-			requesturl = request.url.replace(/\/locate\//g, '').replace(/\//g, '').substring(0, 36);
+		if (request.method === "GET") {
+			var requesturl = request.url.replace(/\/locate\//g, '').replace(/\//g, '').substring(0, 36);
 			redis.hget(config.redis.key, requesturl, function (error, reply) {
 				if (reply) {
 					response.writeHead(303, {
@@ -204,7 +204,7 @@ if (cluster.isMaster) {
 
 				response.end();
 				request.connection.destroy();
-			})
+			});
 		} else {
 			var data = '';
 			request.on('data', function (chunk) {
